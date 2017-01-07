d4962990ea0df22cfd4c56e35717c199aa21e937
huiyunlu <huiyunfiles@gmail.com>
2017-01-08 02:05:52 +0800

    Update proj000002-numtoword.cpp
    Use pointer to character instead of usual array variable for printword function, shorter code
    Capitalize the words

--- proj000002-numtoword.cpp
+++ proj000002-numtoword.cpp
@@ -7,29 +7,28 @@
 
 using namespace std;
 
-void printWord (char w[][20], int d){
-    int i;
+typedef char* pchar;
 
-    for (i=0; w[d][i] != '\0'; i++){
-            cout << w[d][i];
-    }
+void printWord (pchar w){
+    for (; *w != '\0'; w++)
+        cout << *w;
     cout << " ";
 }
 
 int main (){
-    char ones [20][20] = {"", "one", "two", "three", "four",
-                          "five", "six", "seven", "eight", "nine",
-                          "ten", "eleven", "twelve", "thirteen", "fourteen",
-                          "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"},
+    char ones[20][20] = {"", "One", "Two", "Three", "Four",
+                          "Five", "Six", "Seven", "Eight", "Nine",
+                          "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+                          "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"},
 
-         tens [10][20] = {"", "", "twenty", "thirty", "forty",
-                          "fifty", "sixty", "seventy", "eighty", "ninety"},
+         tens[10][20] = {"", "", "Twenty", "Thirty", "Forty",
+                          "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"},
 
-         thou [10][20] = {"", "thousand", "million,", "billion,", "trillion,",
-                          "quadrillion,", "quintillion,", "sextillion,", "gg24,", "gg30,"};
+         thou[10][20] = {"", "Thousand,", "Million,", "Billion,", "Trillion,",
+                          "Quadrillion,", "Quintillion,", "Sextillion,", "gg24,", "gg30,"};
 
     int digits[21] = {0};
-    long int number, n;
+    long long int number, n;
     int i, j;
 /* ****************************************************************************** */
     cout << "Enter a number : ";
@@ -41,24 +40,28 @@ int main (){
 
     for (i = (i-1)/3 * 3; i>=0; i-=3){
 
+        if (digits[i+2] == 0 && digits[i+1] == 0 && digits[i] == 0)
+            continue;
+
         if (digits[i+2] > 0) {
-            printWord (ones, digits[i+2]);
+            printWord (ones[digits[i+2]]);
             cout << "hundred ";
         }
 
         if (digits[i+1] > 0 || digits[i] > 0) {
-            cout << "and ";
 
-            if (digits[i+1] > 1) {
-                printWord (tens, digits[i+1]);
+            if (digits[i+2] > 0) cout << "and ";
+
+            if (digits[i+1] == 1) j = 1;
+            else {
+                printWord (tens[digits[i+1]]);
                 j=0;
                 }
-            else j=1;
 
-            printWord (ones, j * 10 + digits[i]);
+            printWord (ones[j * 10 + digits[i]]);
             }
 
-        printWord (thou, i/3);
+        printWord (thou[i/3]);
 
     }
 
